cf19d655c74b83f49d959d9d1813818e7323715f
Adi Roiban <adi.roiban@chevah.com>
2018-03-07 23:56:31 +0000

    Fix function name.

--- function-1/index.js
+++ function-1/index.js
@@ -4,6 +4,6 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloHttp = (req, res) => {
+exports.receiveCode = (req, res) => {
   res.send(`Hello ${req.body.name || 'World'}!`);
 };
